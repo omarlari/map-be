c1190d237fe4d92870d54fbf5113d975757eaceb
omarlari <omarlari@gmail.com>
2022-09-21 16:22:16 -0700

    fix comments

--- app.py
+++ app.py
@@ -127,24 +127,7 @@ def geoseed():
                                  )
 
 # Insert data into the table
-'''
-    cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('A Tale of Two Cities',
-             'Charles Dickens',
-             489,
-             'A great classic!')
-            )
 
-
-    cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('Anna Karenina',
-             'Leo Tolstoy',
-             864,
-             'Another great classic!')
-            )
-'''
     conn.commit()
 
     cur.close()

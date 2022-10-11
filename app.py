4779c72c9a29d3f8b1ef127c80766e81c2fe7b83
omarlari <omarlari@gmail.com>
2022-10-11 10:29:44 -0700

    this no good

--- app.py
+++ app.py
@@ -92,8 +92,9 @@ def georead():
     maps = cur.fetchall()
     cur.close()
     conn.close()
-    maps.headers.add('Access-Control-Allow-Origin', '*')
-    return maps
+    r = jsonify(maps)
+    r.headers.add('Access-Control-Allow-Origin', '*')
+    return r
 
      
 
